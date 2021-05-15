505d39347a2fab63cb0b63d39a4c5bb60e28f726
Sudhir Kumar Singh <sudhir.manojsingh@gmail.com>
2021-05-15 19:36:15 +0530

    adding logic to print A TO N in alphabateHelper.c file

--- alphabateHelper.c
+++ alphabateHelper.c
@@ -1,4 +1,302 @@
 #include<stdio.h>
+
+// START  a..
+
+
+void printA(int n){
+    
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 || j==n || i==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+}
+// B...
+void printB(int n){
+    
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 || i==n ||j==n || i==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+}
+
+
+// C
+
+
+void printC(int n){
+    
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 || i==n)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+}
+
+//D..
+
+void printD(int n) {
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1||j==n||i==1||i==n)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+//E..
+
+void printE(int n) {
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 || i==n || i==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+
+//F..
+
+void printF(int n) {
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 || i==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+
+//G..
+
+
+void printG(int n) {
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==1 )
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+
+//H..
+void printH(int n){
+    printf("\n");
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1||j==n||i==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+
+//I..
+void printI(int n){
+    printf("\n");
+     for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(i==1||i==n||j==(n/2)+1)
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+// j...
+
+void printJ(int n){
+    printf("\n");
+     for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==n || i==n  )
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+//K...
+
+ void printK(int n){
+    printf("\n");
+     for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i+j==n+1 || j<=(n/2)+1  )
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+// L...
+
+
+void printL(int n){
+    printf("\n");
+     for(int i=1;i<=n;i++)
+    {
+
+        for(int j=1;j<=n;j++)
+        {
+
+           if(j==1 || i==n )
+        {
+         printf("*");
+        }else{
+                printf(" ");
+              }
+        
+    
+        }
+    printf("\n");
+    }
+
+}
+
+
+
+
+ 
+
 void printN(int n){
     printf("\n");
     for(int i=0;i<=n-1;i++)
@@ -243,90 +541,5 @@ for(int i=0;i<=n-1;i++)
         }
     printf("\n");
     }
-}
-void printA(int n){
-    printf("\n");
-    for(int i=0;i<=n-1;i++)
-    {
-
-        for(int j=0;j<=n-1;j++)
-        {
-
-           if(j==0||i==0||j==n-1||i==(n/2))
-        {
-         printf("*");
-        }else{
-                printf(" ");
-              }
-        
-    
-        }
-    printf("\n");
-    }
-
-}
-
-void printI(int n){
-    printf("\n");
-     for(int i=1;i<=n;i++)
-    {
-
-        for(int j=1;j<=n;j++)
-        {
-
-           if(i==1||i==n||j==(n/2)+1)
-        {
-         printf("*");
-        }else{
-                printf(" ");
-              }
-        
-    
-        }
-    printf("\n");
-    }
-
-}
-void printH(int n){
-    printf("\n");
-    for(int i=1;i<=n;i++)
-    {
-
-        for(int j=1;j<=n;j++)
-        {
-
-           if(j==1||j==n||i==(n/2)+1)
-        {
-         printf("*");
-        }else{
-                printf(" ");
-              }
-        
-    
-        }
-    printf("\n");
-    }
-
-}
-void printD(int n) {
-    printf("\n");
-    for(int i=1;i<=n;i++)
-    {
-
-        for(int j=1;j<=n;j++)
-        {
-
-           if(j==1||j==n||i==1||i==n)
-        {
-         printf("*");
-        }else{
-                printf(" ");
-              }
-        
-    
-        }
-    printf("\n");
-    }
-
 }
 
